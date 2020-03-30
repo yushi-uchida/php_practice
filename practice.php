0c2defb1d929bb132466f5c608e7c102ffc2630a
名前 <>
2020-03-30 15:00:58 +0000

    second commit

--- practice.php
+++ practice.php
@@ -8,3 +8,26 @@ $new_a = $a + $b;
 echo $new_a;
 $array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月",];
 echo $array_month[7];
+$hello = "Hello";
+$name = "yushi";
+$world = "'s World!";
+echo $hello.$name.$world;
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+
+$_calendar = [
+    "January" => "1月",
+    "Febryary" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "June" => "6月",
+    "July" => "7月",
+    "August" => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "Nobember" => "11月",
+    "December" => "12月",
+];
+echo $_calendar["December"];
