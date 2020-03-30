cc839016f3dae93519c6763c35339679b5a6f40d
名前 <>
2020-03-30 14:01:45 +0000

    first commit

---
+++ practice.php
@@ -0,0 +1,10 @@
+<?php
+echo 'hello php';
+$a = 3;
+$b = 7;
+echo $a;
+echo $b;
+$new_a = $a + $b;
+echo $new_a;
+$array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月",];
+echo $array_month[7];
