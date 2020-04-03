fa42638ced11ab2c3256c1855d31e84fac78519a
名前 <>
2020-04-03 17:31:53 +0000

    first commit

---
+++ num.php
@@ -0,0 +1,21 @@
+
+<?php
+function num($arr) {
+    $total = 1;
+    foreach($arr as $x) {
+        $total *= $x;
+    }
+    return $total;
+}
+echo num(array(1,3,5,7,9));
+
+function max_array($arr){
+    $max_number = $arr[0];
+    foreach($arr as $a){
+    if($max_number < $a){
+        $max_number = $a;
+    }
+    }
+    return $max_number;
+}
+echo max_array(array(1,2,3,4,5));
