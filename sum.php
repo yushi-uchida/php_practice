006533039d97bc6bdb4521f41aab186537f9fa74
名前 <>
2020-04-03 17:30:22 +0000

    first commit

---
+++ sum.php
@@ -0,0 +1,13 @@
+<?php
+function sum($value) {
+    $result = $value* 2;
+    return $result;
+}
+
+echo sum(100);
+
+function out($a, $b) {
+    $result2 = $a + $b;
+    return $result2;
+}
+echo out(10,10);
